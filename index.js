2369f704f66fde2ad1fba495be98ef3e0d21e63e
deekast <96024516+deekast@users.noreply.github.com>
2024-03-01 02:40:34 -0500

    lesson submission

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let mainElement = document.getElementById('main');
+mainElement.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id='victory';
+
+let yourName = "Danny";
+newHeader.textContent = yourName + " is the champion"
